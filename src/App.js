97d77470db5b6738cc62883b7239dbb02320dfbc
Devin Pastoor <devin.pastoor@gmail.com>
2018-06-02 21:02:22 +0200

    simple app to work on search functionality

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Input} from 'semantic-ui-react';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Input /> 
+      </div>
+    );
+  }
+}
+
+export default App;
