4338d8ca7baccf641f3778e4e507e65f71fa1e91
Devin Pastoor <devin.pastoor@gmail.com>
2018-06-06 06:00:35 -0400

    nonfunctional app trying to figure out search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import {Input} from 'semantic-ui-react';
+import _ from 'lodash'
+import { Search, Grid, Header, Label } from 'semantic-ui-react'
+import TypesenseApi from './api'
+
+let tsa = new TypesenseApi('localhost', "gatsbyserver", "8108", "http")
+
+const resultRenderer = ({ slug }) => {
+return <Label content={ slug } />
+}
+
+
+class SearchExampleCategory extends Component {
+  componentWillMount() {
+    this.resetComponent()
+  }
+
+  resetComponent = () => this.setState({isLoading: false, results: [], value: ''})
+
+  handleResultSelect = (e, {result}) => console.log("selected ", result)
+
+  handleSearchChange = (e, {value}) => {
+    tsa
+      .getSearchResults(value)
+      .then(result => {
+        this.setState({value, results: result})
+      })
+  }
+  render() {
+    const {isLoading, value, results} = this.state
+
+    return (
+      <Grid>
+        <Grid.Column width={8}>
+          <Search
+            category
+            loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 500, {leading: true})}
+            value={value}
+            resultRenderer={(props) => (<div> {JSON.stringify(props, null, 2)} </div>)}
+            minCharacters={2}
+            {...this.props}/>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Header>State</Header>
+          <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
+
 class App extends Component {
+  async fetchAsync() {
+    // await response of fetch call
+    let response = await fetch('https://api.github.com');
+    // only proceed once promise is resolved
+    let data = await response.json();
+    // only proceed once second promise is resolved
+    return data;
+  }
+
   render() {
     return (
       <div className="App">
-       <Input /> 
+        <SearchExampleCategory/>
       </div>
     );
   }
