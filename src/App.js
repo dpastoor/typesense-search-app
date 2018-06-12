b9392b3a310d4445bb3fd3083af171b940935a0b
Devin Pastoor <devin.pastoor@gmail.com>
2018-06-12 14:31:31 -0400

    custom result renderer with some slug options for nav

--- src/App.js
+++ src/App.js
@@ -2,15 +2,61 @@ import React, {Component} from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import _ from 'lodash'
-import { Search, Grid, Header, Label } from 'semantic-ui-react'
+import {Search, Grid, Header, Label, Segment} from 'semantic-ui-react'
 import TypesenseApi from './api'
 
 let tsa = new TypesenseApi('localhost', "gatsbyserver", "8108", "http")
 
-const resultRenderer = ({ slug }) => {
-return <Label content={ slug } />
+const navSlug = (slug, nav_target) => {
+  if (nav_target === "root") {
+     return(slug)
+  }
+  if (slug[slug.length - 1] === "/") {
+    slug = slug.slice(1, -1)
+  }
+  return(slug+nav_target)
 }
+const transformSlug = function (slug) {
+  //assuming each slug has leading and trailing slash
+  if (slug[0] !== "/") {
+    slug = "/" + slug
+  }
+  if (slug[slug.length - 1] !== "/") {
+    slug = slug + "/"
+  }
+  let splits = slug.split("/");
 
+  return (`${splits[1]}/${splits[splits.length - 2]}`)
+}
+const resultRenderer = ({slug, snippets, branch, nav_target}) => {
+  return (
+    <div 
+    style={{
+      display: "block",
+      width: "80vw"
+    }}
+    >
+      <Label content={navSlug(slug, nav_target)}/>
+      <span style={{
+        textAlign: "left"
+      }}>
+      <Segment.Group>
+      {snippets.map((snippet) => {
+      return (
+      <Segment key={snippet}
+      onClick={(e) => {
+        e.preventDefault()
+        console.log(slug, nav_target)
+      }}
+      > 
+      <div dangerouslySetInnerHTML={{__html: snippet}} /> 
+       </Segment>)
+      })}
+      </Segment.Group>
+      </span>
+    </div>
+  )
+}
 
 class SearchExampleCategory extends Component {
   state = {
@@ -33,8 +79,6 @@ class SearchExampleCategory extends Component {
     const {isLoading, value, results} = this.state
 
     return (
-      <Grid>
-        <Grid.Column width={8}>
           <Search
             loading={isLoading}
             onResultSelect={this.handleResultSelect}
@@ -44,12 +88,6 @@ class SearchExampleCategory extends Component {
             results={results}
             minCharacters={2}
             {...this.props}/>
-        </Grid.Column>
-        <Grid.Column width={8}>
-          <Header>State</Header>
-          <pre>{JSON.stringify(this.state, null, 2)}</pre>
-        </Grid.Column>
-      </Grid>
     )
   }
 }
